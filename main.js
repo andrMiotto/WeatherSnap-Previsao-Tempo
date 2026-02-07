233fb3f268d6286d1f825eac42ec2bc64dde86d1
andrMiotto <andre_lm_pereira@estudante.sesisenai.org.br>
2026-02-06 21:54:22 -0300

    feat:termina-maioria

--- main.js
+++ main.js
@@ -1,16 +1,47 @@
 const tempElemento = document.querySelector(".temperature");
+const windElemento = document.querySelector(".wind");
+const humidityElemento = document.querySelector(".humidity");
+const cityElemento = document.querySelector(".city");
+const cidadeDigitada = document.querySelector(".searching-city");
+const botaoBuscar = document.querySelector(".search-button");
 
 
 const apiKey = "195dc487f4791e835d087050c4c4365d";
-const cidade = "São Paulo";
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&units=metric&lang=pt_br`;
+ 
 
-fetch(url)
-    .then((response) => {
-        if (!response.ok) throw new Error("Erro ao buscar dados");
-        return response.json();
-    })
-    .then((data) => {
-        console.log(`A temperatura em ${data.name} é de ${data.main.temp}°C`);
-    })
-    .catch((error) => console.error("Erro:", error));
+botaoBuscar.addEventListener("click", () => {
+    const cidade = cidadeDigitada.value;
+
+    if (cidade === "") {
+        alert("Digite uma cidade");
+        return;
+    }
+
+
+
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&units=metric&lang=pt_br`;
+
+    fetch(url)
+        .then((response) => {
+            if (!response.ok) throw new Error("Cidade não encontrada");
+            return response.json();
+        })
+
+
+        .then((data) => {
+            tempElemento.textContent = Math.floor(data.main.temp) + "°C";
+            windElemento.textContent = Math.floor(data.wind.speed) + " Km/h";
+            humidityElemento.textContent = Math.floor(data.main.humidity) + " %";
+            cityElemento.textContent = data.name;
+
+            cidadeDigitada.value = "";
+
+
+        })
+
+        .catch((error) => console.error("Erro:", error));
+
+
+
+});
